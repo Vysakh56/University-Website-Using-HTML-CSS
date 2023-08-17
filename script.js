cb456faf54e47df496335b42d32cf8f48254ee28
Vysakh Venugopalan <vysakhvenugopalan@gmail.com>
2023-08-17 23:17:20 +0530

    js file added

---
+++ script.js
@@ -0,0 +1,11 @@
+var navLinks = document.getElementById("navLinks");
+
+// For Toggle Menu
+
+function ShowMenu(){
+    navLinks.style.right = "0";
+}
+
+function HideMenu(){
+    navLinks.style.right = "-200px";
+}
